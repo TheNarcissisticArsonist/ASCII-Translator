1412cdcda0d782185233e762336e9fcd229b4d1f
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-10 22:25:06 -0700

    Reads the first word.

--- ASCII-Translator.cpp
+++ ASCII-Translator.cpp
@@ -7,9 +7,14 @@ int main() {
 
 	cout << "ASCII Translator" << endl;
 	cout << "Input text, and I'll convert it to numbers!" << endl;
+
 	std::string wrongTypeUserInput;
 	cin >> wrongTypeUserInput;
 	const char* userInput = wrongTypeUserInput.c_str();
+
+	for(int i=0; i<wrongTypeUserInput.size(); ++i) {
+		cout << static_cast<int>(userInput[i]) << endl;
+	}
 	cout << userInput << endl;
 
 	return 0;
